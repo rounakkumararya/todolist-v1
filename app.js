3f9061794898a218a58f4586ccb04442cb0ad9ba
Rounak <rounakkumar040@gmail.com>
2023-04-28 20:59:03 +0530

    adding new list items into webpage by user

--- app.js
+++ app.js
@@ -6,45 +6,33 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var items = [];
 
 app.get("/", function (req, res) {
     var today = new Date();
 
-    // oloder option for date
-    // var currentDay = today.getDay();
-    // var day = "";
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    // switch (currentDay) {
-    //     case 0:
-    //         day = "sunday";
-    //         break;
-    //     case 1:
-    //         day = "monday";
-    //         break;
-    //     case 2:
-    //         day = "tuesday";
-    //         break;
-    //     case 3:
-    //         day = "wednesday";
-    //         break;
-    //     case 4:
-    //         day = "thrusday";
-    //         break;
-    //     case 5:
-    //         day = "friday";
-    //         break;
-    //     case 6:
-    //         day = "saturday";
-    //         break;
-    //     default:
-    //         console.log("Error: current day is equal to:" + currentDay);
-}
-    res.render("list", { kindOfDay: day });
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list", {
+        kindOfDay: day,
+        newListItem: items
+    });
 });
 
-
-
-
+app.post("/", function (req, res) {
+    var item = req.body.newItem
+    items.push(item);
+    res.redirect("/")
+})
 
 
 
